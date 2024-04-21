4270ca3a6fa06b90b9b0457e9a315ef0c0f14b8d
Aleksey Pardaev <lpardaev607@gmail.com>
2024-04-21 22:08:21 +0300

    1

--- src/main/java/com/example/servlet/LoginServlet.java
+++ src/main/java/com/example/servlet/LoginServlet.java
@@ -1,5 +1,37 @@
 package com.example.servlet;
 
-public class LoginServlet {
-    //write your code here!
+import com.example.Users;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        String login = (String) req.getParameter("login");
+        String password = (String) req.getParameter("password");
+
+        if (password != null && !password.trim().isEmpty() && Users.getInstance().getUsers().contains(login)) {
+            session.setAttribute("user", login);
+            req.getRequestDispatcher("/user/hello.jsp").forward(req, resp);
+        } else {
+            req.getRequestDispatcher("/login.jsp").forward(req, resp);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if(req.getSession().getAttribute("user") == null) {
+            req.getRequestDispatcher("/login.jsp").forward(req, resp);
+        } else {
+            req.getRequestDispatcher("/user/hello.jsp").forward(req, resp);
+        }
+    }
 }
